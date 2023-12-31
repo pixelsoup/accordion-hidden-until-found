97eab3611e44b6621e040224a9f343ac0128a3c2
Geoff Schwartz <eieio@pixelsoup.com.au>
2023-12-31 15:46:15 +1100

    js: use single quotes

--- js/development.js
+++ js/development.js
@@ -1,24 +1,24 @@
-document.querySelectorAll(".js-accordion").forEach((accordion) => {
+document.querySelectorAll('.js-accordion').forEach((accordion) => {
   accordion.onbeforematch = () => {
-    accordion.classList.remove("collapsed")
+    accordion.classList.remove('collapsed')
   }
 
 
-  accordion.querySelector(".js-acTrigger").onclick = () => {
+  accordion.querySelector('.js-acTrigger').onclick = () => {
 
     // toggle class collapsed on click
-    accordion.classList.toggle("collapsed")
+    accordion.classList.toggle('collapsed')
 
     // define accordion pane dom node
-    const details = accordion.querySelector(".js-details")
+    const details = accordion.querySelector('.js-details')
 
-    // if accordion contains class "collapsed" add value attribute hidden = "until-found"
-    if (accordion.classList.contains("collapsed")) {
-      details.hidden = "until-found"
+    // if accordion contains class 'collapsed' add value attribute hidden = 'until-found'
+    if (accordion.classList.contains('collapsed')) {
+      details.hidden = 'until-found'
     } else {
 
       // remove attribute hidden
-      details.removeAttribute("hidden")
+      details.removeAttribute('hidden')
     }
   }
 })
